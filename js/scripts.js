f621c0408a771f1a1d136ac1c8e376ab9e81b052
Ben Schenkenberger and Xi Xia <ben@>
2017-02-27 10:27:21 -0800

    add html elements for receiving task properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
-$(document).ready(function (){
+//business logic
 
+//sample object Task
+// var task1 = {
+//   name: "",
+//   dueDate: //calendar form input,
+//   note: "",
+//   typeOfTask: selector
+// };
+
+//task constructor
+var newTask = function (name, dueDate, note, typeOfTask){
+  this.nameOfTask = name;
+  this.taskDueDate = dueDate;
+  this.taskNote = note;
+  this.taskType = typeOfTask;
+}
+
+
+$(document).ready(function () {
+  $("#toDoList").submit(function(event) {
+    event.preventDefault();
+    var inputtedTask = $("input#tasks").val();
+
+    // error checking for blank field
+    if (inputtedTask === "") {
+      alert("You forgot your task!")
+    }
+    else {
+      $("#displayList").append("<h3>"+inputtedTask+"</h3>");
+
+        $("#toDoList").trigger("reset");
+    }
+  });
 });
